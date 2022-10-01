8e32613432515c187c425146d3eb5fd12c92acae
pxmysza <pawemy@gmail.com>
2022-10-01 21:15:25 +0200

    project rewritten

---
+++ functionality/json_converter.py
@@ -0,0 +1,20 @@
+from functionality.sentence import Sentence
+import json
+
+
+class JsonConverter:
+    @staticmethod
+    def convert_to_json(text: Sentence):
+        """Converts 'Sentence' object to 'json'"""
+        payload = {
+            "cipher_type": text.cipher_type,
+            "text": text.text,
+            "status": text.status
+        }
+        return json.dumps(payload, indent=4)
+
+    @staticmethod
+    def convert_from_json(text: dict) -> Sentence:
+        """Converts 'json' object to 'Sentence' object"""
+        s = Sentence(text["cipher_type"], text["text"], text["status"])
+        return s
---
+++ functionality/rot_factory.py
@@ -0,0 +1,10 @@
+from functionality.rot import Rot47, Rot13
+
+class RotFactory:
+
+    @classmethod
+    def get_rot(cls, cipher_type: str, text: str):
+        if cipher_type == "rot47":
+            return Rot47(text)
+        elif cipher_type == "rot13":
+            return Rot13(text)
